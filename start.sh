e468be553484a20aface8954f72114ae5ca125bf
Aadhixr <129429716+Aadhixr@users.noreply.github.com>
2023-05-09 08:08:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Aadhi000/vnc.git /vnc
+  git clone https://github.com/Aadhixr/vnc.git /vnc
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /vnc
